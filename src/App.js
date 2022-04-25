d87b3275538482763c3882f0f4c85fc90568070d
Leonardo Araujo <leo.san9@hotmail.com>
2022-04-25 10:28:44 -0300

    pastas, arquivos e nav bar iniciados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { NavBar } from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <NavBar />
         <Routes>
           <Route path='/'/>
         </Routes>
