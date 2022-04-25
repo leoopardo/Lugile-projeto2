b622e20d2d582a23e5a136463bafa3b828e66b19
Leonardo Araujo <leo.san9@hotmail.com>
2022-04-25 14:09:36 -0300

    searchBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { NavBar } from './components/NavBar/NavBar';
+import { useState, useEffect } from "react"
+import axios from 'axios';
 
 function App() {
+  const [itens, setItens] = useState([])
+  const [backUp, setBeckUp] = useState([...itens])
+
+  useEffect(() => {
+    async function getItens(){
+      const response = await axios.get('https://fakestoreapi.com/products');
+      setItens(response.data);
+    }
+    getItens()
+  }, [])
+
+  console.log(itens)
+  function filterItems(filterParams){
+    if (filterParams === ""){
+      setItens(backUp);
+      return;
+    }
+
+    const filtred = itens.filter((currentItem) => {
+      return (currentItem.title || currentItem.description || currentItem.category)
+      .toLowerCase()
+      .includes(filterParams.toLowerCase());
+    });
+    setItens(filtred)
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
-      <NavBar />
+      <NavBar filterState={filterItems}/>
         <Routes>
           <Route path='/'/>
         </Routes>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,20 @@
 import "./NavBar.css"
-export function NavBar() {
+
+export function NavBar(props) {
     return ( 
         <nav className="NavBar">
+            <div className="SearchBar">
+                <form className="FormSearchBar">
+                    <input 
+                        type="text"
+                        className="SearchBar"   
+                        placeholder= "pesquise"
+                        onKeyUp={(e) => {props.filterState(e.target.value)
+                        }}
+                    />
+                </form>     
+            </div>
         </nav>
+    
      );
 }
