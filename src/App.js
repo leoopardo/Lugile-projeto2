9aed7c4f6b799509b96ff7365d41fd7b66355238
Lucas Reis <lucaskreis.com>
2022-04-25 15:11:00 -0300

    primeiro commit da home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { NavBar } from './components/NavBar/NavBar';
+import {Home} from "./pages/Home/Home"
+import {Product} from "./pages/Product/Product"
 
 function App() {
   return (
@@ -8,7 +10,8 @@ function App() {
       <BrowserRouter>
       <NavBar />
         <Routes>
-          <Route path='/'/>
+        <Route path="/" element={<Home/>} />
+        <Route path="/produto/:prod" element={<Product/>}/>
         </Routes>
       </BrowserRouter>
     </div>
