fb8c25e5612ab5e52dca686fe6da7cfdb31a1a9b
Gian Lucca <giandacoll@gmail.com>
2022-04-25 13:56:01 -0300

    "alterações"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import { NavBar } from './components/NavBar/NavBar';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {NavBar} from "./Components/Navbar/NavBar"
+import { Cadastro } from './Components/Navbar/Cadastro/Cadastro';
+import { Login } from './Components/Navbar/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <NavBar />
-        <Routes>
-          <Route path='/'/>
+      <NavBar/>
+          <Routes>
+            <Route path='/'/>
+            <Route path='/cadastro' element={<Cadastro/>}/>
+            <Route path='/login' element={<Login />}/>
+
+
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
 import "./NavBar.css"
+
+
 export function NavBar() {
     return ( 
         <nav className="NavBar">
+
+           <Link to="/cadastro" ><button> Cadastro </button></Link>
+           <Link to="/login" ><button> Login </button></Link>
+
+        
         </nav>
      );
 }
