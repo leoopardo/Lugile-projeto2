f05dccfc042a77dfb568a5f8c910b70e891879f3
Gian Lucca <giandacoll@gmail.com>
2022-04-26 09:54:24 -0300

    login done by id

--- src/components/NavBar/Login/Login.js
+++ src/components/NavBar/Login/Login.js
@@ -1,24 +1,52 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
+    const navigate = useNavigate();
+    const [password, setPassword] = useState("");
+    const [login, setLogin] = useState([]);
+
+    
+    useEffect(() => {
+        async function getPassword(){
+            const response = await axios.get(`https://ironrest.herokuapp.com/findOne/Lugile-usuários?password=${password}`)
+            setLogin(response.data)
+        }
+        getPassword();
+    }, [password])
+
+
+    function handleSubmit(e){
+        e.preventDefault()
+        navigate(`/userpage/${login._id}`)
+
+
+    }
   
         return ( 
 
 
-        <div>               
+        <div>
+            <form onSubmit={handleSubmit} >              
                 <input 
                     name="name"
                     type="text"
                     placeholder="nome"
+                    
+
                 />
                 
                 <input 
                     name="password"
                     type="password"
                     placeholder="senha"
+                    value={password}
+                    onChange={(e)=> setPassword(e.target.value)}
                 /> 
                 
-               <Link to="/userpage"> <button>Login</button> </Link>
+                <button type="submit"> Login </button> 
+            </form> 
  
         </div>
          
